8256eb43ae7fd94ec7ec9b5b0c8086e07e9eef6e
joaogabrielts <teixeirajsilva21@gmail.com>
2024-01-25 09:32:34 -0300

    Finalizando atividade

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2){
+    if(player1 == "Pedra" && player2 == "Pedra"){
+        console.log("Empate!")
+        return "Empate!"
+    }
+    else if(player1 == "Pedra" &&  player2 == "Tesoura"){
+        console.log("Jogador 1 venceu!")
+        return "Jogador 1 venceu!"
+    }  
+    else if(player1 == "Pedra" && player2 == "Papel"){
+         console.log("Jogador 2 venceu!")
+         return "Jogador 2 venceu!"
+    }    
+    else if(player1 == "Papel" &&  player2 == "Papel"){    console.log("Empate!")
+         return "Empate!"
+    }
+    else if(player1 == "Papel" && player2 == "Pedra"){
+        console.log("Jogador 1 venceu!")
+        return "Jogador 1 venceu!"
+    }
+    else if(player1 == "Papel" && player2 == "Tesoura"){
+        console.log("Jogador 2 venceu!")
+        return "Jogador 2 venceu!"
+    }
+
+    else if(player1 == "Tesoura" && player2 == "Tesoura"){
+        console.log("Empate!")
+        return "Empate!"
+    }   
+    } 
+    
+
+const resultado = playRockPaperScissor("Pedra", "Papel");
+console.log(resultado)
+
+
+ 
+  
+    
